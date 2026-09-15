82ff170c8f7bbd92e04eb439f8481e887ecb9e5c
Hubert Krajnik <167282127+HubertKr4jnik@users.noreply.github.com>
2026-09-15 13:21:49 +0200

    add adding issues functionality

--- app/page.tsx
+++ app/page.tsx
@@ -49,9 +49,31 @@ export default function Home() {
     "Malaga Centro Alameda",
     "Malaga Centro 02",
   ]);
+  const [priorities, setPriorities] = useState<string[]>([
+    "low",
+    "medium",
+    "high",
+  ]);
+  const [categories, setCategories] = useState<string[]>([
+    "plumbing",
+    "electricity",
+    "cleaning",
+    "furniture",
+    "internet",
+  ]);
+  const [apartmentToAdd, setApartmentToAdd] = useState("");
+  const [categoryToAdd, setCategoryToAdd] = useState("");
+  const [descToAdd, setDescToAdd] = useState("");
+  const [dateToAdd, setDateToAdd] = useState(new Date());
+  const [priorityToAdd, setPriorityToAdd] = useState("");
+  const [isOverlayVisible, setIsOverlayVisible] = useState(false);
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-col bg-white dark:bg-black">
+        {isOverlayVisible && (
+          <div className="absolute top-0 left-0 w-full h-screen z-1 bg-[rgba(0,0,0,.5)]"></div>
+        )}
+
         <h1 className="font-bold text-2xl">Issues:</h1>
         <div className="flex justify-center flex-wrap gap-10">
           {issues.length > 0 ? (
@@ -89,10 +111,95 @@ export default function Home() {
         {!isAddingIssue && (
           <input
             type="button"
-            onClick={() => setIsAddingIssue(true)}
+            onClick={() => {
+              setIsAddingIssue(true);
+              setIsOverlayVisible(true);
+            }}
             value="Add issue"
             className="w-fit px-2 py-1 cursor-pointer border hover:font-semibold rounded transition-all"
           />
+        )}
+        {isAddingIssue && (
+          <div className="absolute flex flex-col place-items-center z-3 p-6 gap-4 w-4/5 md:w-2/3 lg:w-1/3 h-fit top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-black border-2 border-slate-500 rounded">
+            <h2 className="font-semibold text-xl">Add new issue:</h2>
+            <div className="flex gap-2">
+              <span>Apartment: </span>
+              <select
+                onChange={(e) => setApartmentToAdd(e.target.value)}
+                className="border-b"
+              >
+                {apartments.map((apartment, index) => (
+                  <option key={index} value={apartment} className="text-black">
+                    {apartment}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="flex gap-2">
+              <span>Caegory: </span>
+              <select
+                onChange={(e) => setCategoryToAdd(e.target.value)}
+                className="border-b"
+              >
+                {categories.map((category, index) => (
+                  <option key={index} value={category} className="text-black">
+                    {category}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex gap-2">
+              <span>Priority: </span>
+              <select
+                onChange={(e) => setPriorityToAdd(e.target.value)}
+                className="border-b"
+              >
+                {priorities.map((priority, index) => (
+                  <option key={index} value={priority} className="text-black">
+                    {priority}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="flex gap-2">
+              <span>Date: </span>
+              <input
+                type="date"
+                onChange={(e) => setDateToAdd(new Date(e.target.value))}
+                className="border-b"
+              />
+            </div>
+            <div className="flex gap-2">
+              <span>Description: </span>
+              <textarea
+                value={descToAdd}
+                onChange={(e) => setDescToAdd(e.target.value)}
+                className="border rounded p-1"
+              ></textarea>
+            </div>
+            <input
+              type="button"
+              value="Add"
+              onClick={() => {
+                setIsOverlayVisible(false);
+                setIsAddingIssue(false);
+                setIssues([
+                  ...issues,
+                  {
+                    id: apartments.length,
+                    apartment: apartmentToAdd,
+                    category: categoryToAdd,
+                    desc: descToAdd,
+                    date: dateToAdd,
+                    priority: priorityToAdd,
+                    status: "pending",
+                  },
+                ]);
+              }}
+              className="w-fit px-4 py-1 cursor-pointer border hover:font-semibold rounded transition-all"
+            />
+          </div>
         )}
       </main>
     </div>
